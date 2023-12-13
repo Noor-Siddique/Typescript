c956d1299de91e9accadb5b3fc7084c5cf3b7b77
Noor-Siddique <153700256+Noor-Siddique@users.noreply.github.com>
2023-12-13 02:42:46 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// let marks = [15, 25, 35, 45, 55, 65, 75];
+// console.log(marks);
+// console.log(marks.length); // ya jo length hai ya hamari property
+//hoti hai arryas ky anadr property or methods may difference hota hai
+// property hoti hai jo kuch value dy daiti hai method hota hai jo kuch kam krta hai
+//yaha pay length koi kam nhi kr rahi bs aik value hai jo stored hai hr array ky sth
+// us ki length ki property store hoti hai
+// array apny ap may koi type nhi hota . array khud aik object type hota hai
+// jb hm ny data type ki bat ki thi to hamary pas hoty thy 2 taraha ky data types
+//aik hoty hai premetive matlab ky basic data types or dosry hoty thy 
+//non premetive matlab advanced data type so advance data type ky andar hm ny 
+//object ki bat ki thi array bhi aik special type of object hi hoti hai
+// array internally javascript ky andar aik object hi hoti hai
+//arryas ko objects ky comparision may hm seperatly treat krty hai
+// to hm ya soch skty hai array hamari asi object hai jis may key: value pair
+//to hota hai but key ki jaga hm index ko use krty hai or index ko hm array may property bhi
+//kehty hai
+//array indices 
+// indices matlab index ki plural
+// let marks = [15, 25, 35, 45, 55, 65, 75];
+//            0   1   2   3   4   5   6 so this method basically value stored in linear fashion
+// linear fashion matlab single line fshion may value store hoti hai
+// let marks = [15, 25, 35, 45, 55, 65, 75];
+// console.log(marks[3]);
+//jb hm wo index access krty hai jo array ky andar access hi nhi krta to hamy
+//return may undefined print krta hai also agar kabhi hamy array ki value ko change
+//krna hota hao so hm usy bhi ja ky change kr skty hai simply kisi bhi index may ja ky
+// mujhy value ko change krna hui for example mujhy 35 ky bajaye 36 krna hia to
+// hm simply likh sty hai jaisy marks phir index value[2]=27                 
+var marks = [15, 25, 35, 45, 55, 65, 75];
+console.log(marks[2] = 37); // so the ans 37 yeah
+var firstName = ["Noor", "Fatima", "Siddique", "Akbar"];
+console.log(firstName[1] = "Ayesha");
